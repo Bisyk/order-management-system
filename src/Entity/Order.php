e5e7ac2d66378a1ba5657b7d73596ec33c4e3238
Bisyk <99905134+Bisyk@users.noreply.github.com>
2025-10-02 15:50:17 +0300

    setup Doctrine ORM and create Order Entity

---
+++ src/Entity/Order.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\OrderRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: OrderRepository::class)]
+#[ORM\Table(name: '`orders`')]
+class Order
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $customer_name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $customer_email = null;
+
+    #[ORM\Column(type: Types::SMALLINT)]
+    private ?int $total_amount = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $status = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $created_at = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $updated_at = null;
+
+    public function __construct() {}
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCustomerName(): ?string
+    {
+        return $this->customer_name;
+    }
+
+    public function setCustomerName(string $customer_name): static
+    {
+        $this->customer_name = $customer_name;
+
+        return $this;
+    }
+
+    public function getCustomerEmail(): ?string
+    {
+        return $this->customer_email;
+    }
+
+    public function setCustomerEmail(string $customer_email): static
+    {
+        $this->customer_email = $customer_email;
+
+        return $this;
+    }
+
+    public function getTotalAmount(): ?int
+    {
+        return $this->total_amount;
+    }
+
+    public function setTotalAmount(int $total_amount): static
+    {
+        $this->total_amount = $total_amount;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): static
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $created_at): static
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updated_at;
+    }
+
+    public function setUpdatedAt(\DateTimeImmutable $updated_at): static
+    {
+        $this->updated_at = $updated_at;
+
+        return $this;
+    }
+}
